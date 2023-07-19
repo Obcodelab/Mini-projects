6e04ac0c72d674cd9580be844e06c0a242045c2e
OB <gideonayomide75@gmail.com>
2023-07-19 13:22:40 +0100

    updated with Enter keybind

--- age_calculator/main.py
+++ age_calculator/main.py
@@ -1,44 +1,54 @@
-#Imports required module
-from tkinter import *
+# Imports required module
 from datetime import datetime
+from tkinter import *
 
-#Gets year
+# Gets year
 current_year = datetime.now().year
 
-#Calculates age
+
+# Calculates age
 def calculate():
     dob = dateInput.get()
     if dob.isdigit():
         dob = int(dob)
         age = current_year - dob
-        result.configure(text = age)
+        result.configure(text=age)
     else:
-        result.configure(text = 'Please enter a integer value')
+        result.configure(text="Please enter a integer value")
 
-#Creates a window
+
+# Calls dateBtn
+def on_enter_key(event):
+    dateBtn.invoke()
+
+
+# Creates a window
 root = Tk()
-root.title('Age Calculator')
-root.geometry('300x100')
+root.title("Age Calculator")
+root.geometry("300x100")
 
-#Year Label
+# Enter Key bind
+root.bind("<Return>", on_enter_key)
+
+# Year Label
 date = Label(root, text="Year of birth : ")
 date.grid(row=0, column=0, padx=5, pady=5)
 
-#Year Entry
+# Year Entry
 dateInput = Entry(root, width=20)
 dateInput.grid(row=0, column=1, padx=5, pady=5)
 
-#Calculate Button
-dateBtn = Button(root, text="Calculate", width=8,command=calculate)
+# Calculate Button
+dateBtn = Button(root, text="Calculate", width=8, command=calculate)
 dateBtn.grid(row=2, column=1)
 
-#Age Label
+# Age Label
 resultLabel = Label(root, text="Age : ")
-resultLabel.grid(row=1,column=0, padx=5, pady=5)
+resultLabel.grid(row=1, column=0, padx=5, pady=5)
 
-#Age
+# Age
 result = Label(root, text="")
 result.grid(row=1, column=1, padx=5, pady=5)
 
-#Initializes the program
+# Initializes the program
 root.mainloop()
--- age_calculator/main2.py
+++ age_calculator/main2.py
@@ -1,44 +1,55 @@
-#Imports required module
-from customtkinter import *
+# Imports required module
 from datetime import datetime
 
-#Gets year
+from customtkinter import *
+
+# Gets year
 current_year = datetime.now().year
 
-#Calculates age
+
+# Calculates age
 def calculate():
     dob = dateInput.get()
     if dob.isdigit():
         dob = int(dob)
         age = current_year - dob
-        result.configure(text = age)
+        result.configure(text=age)
     else:
-        result.configure(text = 'Please enter a integer value')
+        result.configure(text="Please enter a integer value")
 
-#Creates a window
+
+# Calls dateBtn
+def on_enter_key(event):
+    dateBtn.invoke()
+
+
+# Creates a window
 root = CTk()
-root.title('Age Calculator')
-root.geometry('300x100')
+root.title("Age Calculator")
+root.geometry("300x100")
 
-#Year Label
+# Enter Key bind
+root.bind("<Return>", on_enter_key)
+
+# Year Label
 date = CTkLabel(root, text="Year of birth : ")
 date.grid(row=0, column=0, padx=5)
 
-#Year Entry
+# Year Entry
 dateInput = CTkEntry(root, width=150)
 dateInput.grid(row=0, column=1, padx=5)
 
-#Calculate Button
-dateBtn = CTkButton(root, text="Calculate", width=50,command=calculate)
+# Calculate Button
+dateBtn = CTkButton(root, text="Calculate", width=50, command=calculate)
 dateBtn.grid(row=2, column=1)
 
-#Age Label
+# Age Label
 resultLabel = CTkLabel(root, text="Age : ")
-resultLabel.grid(row=1,column=0, padx=5)
+resultLabel.grid(row=1, column=0, padx=5)
 
-#Age
+# Age
 result = CTkLabel(root, text="")
 result.grid(row=1, column=1, padx=5)
 
-#Initializes the program
+# Initializes the program
 root.mainloop()
