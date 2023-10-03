1916387c0a9b36110d3199d8f533d1634f66dbff
obcodelab <gideonayomide75@gmail.com>
2023-10-03 18:50:31 +0100

    updated with logging functionality, language detection and better error output

--- audio_transcriber/main.py
+++ audio_transcriber/main.py
@@ -1,5 +1,10 @@
+import logging
 import os
 
+# Set up logging
+logging.basicConfig(filename="transcription.log", level=logging.INFO)
+
+# Checks if assemblyai is installed
 print("[*] Checking if assemblyai is installed.....")
 
 try:
@@ -22,18 +27,34 @@ aai.settings.api_key = f"API_KEY_HERE"
 # URL of the file to transcribe
 FILE_URL = "FILE_PATH_HERE"
 
-# Transcribe the file
-transcriber = aai.Transcriber()
-transcript = transcriber.transcribe(FILE_URL)
+try:
+    # Create a new TranscriptionConfig
+    config = aai.TranscriptionConfig(language_detection=True)
 
-# Formats the text
-text = transcript.text
-sentences = text.split(". ")
-formatted_text = "\n".join(sentences)
+    # Transcribe the file
+    transcriber = aai.Transcriber(config=config)
+    transcript = transcriber.transcribe(FILE_URL)
 
-# Saves the transcription to a file
-with open("FILE_NAME.txt", "w") as f:
-    f.write(formatted_text)
+    # Checks for errors
+    if transcript.status == aai.TranscriptStatus.error:
+        print(f"Transcription failed: {transcript.error}")
+
+    # Formats the text
+    text = transcript.text
     print("[*] Transcription complete")
+    sentences = transcript.get_sentences()
+    formatted_sentences = []
+    for sentence in sentences:
+        formatted_text = sentence.text
+        formatted_sentences.append(formatted_text)
 
-print("[*] Transcription saved to FILE_NAME.txt")
+    # Saves the transcription to a file
+    with open("FILE_NAME.txt", "w") as f:
+        f.write(formatted_text + "\n")
+        print("[*] Transcription complete")
+
+    print("[*] Transcription saved to FILE_NAME.txt")
+
+except Exception as e:
+    print(f"Transcription failed: {e}")
+    logging.error("Error during transcription", exc_info=True)
